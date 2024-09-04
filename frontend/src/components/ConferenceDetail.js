be89ad3f9f8ad3c4b8a910837d96403ba2db146d
Max Arellano <maxfrda@gmail.com>
2024-09-04 12:15:00 -0500

    Fix layout

--- frontend/src/components/ConferenceDetail.js
+++ frontend/src/components/ConferenceDetail.js
@@ -1,8 +1,6 @@
-// src/components/ConferenceDetail.js
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { formatDate } from '../utils/dateUtils'; // Import the utility function
-
+import { formatDate } from '../utils/dateUtils'; 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const ConferenceDetail = () => {
@@ -22,7 +20,7 @@ const ConferenceDetail = () => {
   return (
     <>
       <div className='w-3/4 mx-auto'>
-      <div className="relative">
+        <div className="relative">
           <img
             src="https://emt.gartnerweb.com/ngw/commonassets/images/vis-imagery/photography/conference-homepage-banners/symposium-color-02.png"
             alt="Conference Calendar"
@@ -30,24 +28,23 @@ const ConferenceDetail = () => {
           />
           <div className="absolute top-1/2 left-0 transform -translate-y-1/2 pl-12 max-w-2xl">
             <h1 className="text-7xl text-white font-bold mb-4">
-             {conference.title}
+              {conference.title}
             </h1>
             <p className="text-3xl text-white">
             {formatDate(conference.date)} | {conference.location}
             </p>
             <button className="bg-orange-600 text-white mt-4 px-6 py-4 text-2xl ">
-        View Agenda
-      </button>
-
+              View Agenda
+            </button>
           </div>
         </div>
       </div>
 
       <div className="max-w-4xl mx-auto p-6 py-24 rounded-lg">
-      <h1 className="text-5xl font-bold mb-4">{conference.subtitle}</h1>
-      <p className="text-gray-700 mb-4 text-2xl">{conference.description}</p>
-      <a href="/" className="text-blue-600 hover:underline">Back to list</a>
-    </div>
+        <h1 className="text-5xl font-bold mb-4">{conference.subtitle}</h1>
+        <p className="text-gray-700 mb-4 text-2xl">{conference.description}</p>
+        <a href="/" className="text-blue-600 hover:underline">Back to list</a>
+      </div>
     </>
 
   );
--- frontend/src/components/ConferenceList.js
+++ frontend/src/components/ConferenceList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { formatDate } from '../utils/dateUtils'; // Import the utility function
+import { formatDate } from '../utils/dateUtils'; 
 import ConferenceModal from './ConferenceModal';
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
@@ -51,89 +51,87 @@ const ConferenceList = () => {
   return (
     <>
     <div className="relative">
-          <img
-            src="https://emt.gartnerweb.com/ngw/commonassets/images/build-graphics/calendar-new-viz/conference-calendar-hero-desktop-new.jpg"
-            alt="Conference Calendar"
-            className="w-full h-auto object-cover"
-          />
-          <div className="absolute top-1/2 left-0 transform -translate-y-1/2 pl-12 max-w-2xl">
-            <h1 className="text-7xl text-white font-bold mb-4">
-              Conference Calendar
-            </h1>
-            <p className="text-3xl text-white">
-              Experience IT and business conferences aligned to your role and optimized to help you achieve stronger performance on your organization’s mission-critical priorities.
-            </p>
-          </div>
-        </div>
-
-<div className="flex flex-col sm:flex-row items-start justify-between py-24 px-64 bg-white">
-  <div className="max-w-[400px] mb-4 sm:mb-0">
-    <h3 className="text-4xl mb-2 text-custom-blue font-semibold">
-      Learn More about Gartner conferences
-    </h3>
-    <p className="text-xl text-gray-600">
-      Get our latest conference schedules, updates, and insights straight to your inbox.
-    </p>
-  </div>
-  <div className="flex flex-col  space-y-2 max-w-[600px]">
-    <p className="text-xl text-custom-blue font-semibold">Work Email</p>
-    <div className="flex items-center space-x-4 mb-2">
-      <input 
-        type="email" 
-        placeholder="Enter your work email" 
-        className="p-2 border border-gray-300 rounded-md flex-1"
+      <img
+        src="https://emt.gartnerweb.com/ngw/commonassets/images/build-graphics/calendar-new-viz/conference-calendar-hero-desktop-new.jpg"
+        alt="Conference Calendar"
+        className="w-full h-auto object-cover"
       />
-      <button className="bg-custom-blue text-white px-4 py-2 rounded-md">
-        Continue
-      </button>
+      <div className="absolute top-1/2 left-0 transform -translate-y-1/2 pl-12 max-w-2xl">
+        <h1 className="text-7xl text-white font-bold mb-4">
+          Conference Calendar
+        </h1>
+        <p className="text-3xl text-white">
+          Experience IT and business conferences aligned to your role and optimized to help you achieve stronger performance on your organization’s mission-critical priorities.
+        </p>
+      </div>
     </div>
-    <p className="text-sm text-gray-600 max-w-full">
-      By clicking the "Subscribe" button, you are agreeing to the Gartner Terms of Use and Privacy Policy.
-    </p>
-  </div>
-</div>
-    <div className="space-y-6 px-64 py-24 ">
-    <h2 className="text-5xl font-semibold text-gray-800 mb-4">Find your conference</h2>
-    <button className="bg-custom-blue text-white px-6 py-2 rounded-md hover:bg-blue-700" 
-    onClick={handleAddConferenceClick}
->
-        Add Conference
-      </button>
-    <ul className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      {conferences.map((conference) => (
-        <li 
-          key={conference.id} 
-          className="bg-white rounded-lg shadow-md overflow-hidden min-h-[185px] min-w-[280px] flex flex-col justify-between hover:bg-gray-200"
 
-        >
-          <Link 
-            to={`/conferences/${conference.id}`} 
-            className="block p-4 flex flex-col justify-between h-full"
+    <div className="flex flex-col sm:flex-row items-start justify-between py-24 px-64 bg-white">
+      <div className="max-w-[400px] mb-4 sm:mb-0">
+        <h3 className="text-4xl mb-2 text-custom-blue font-semibold">
+          Learn More about Gartner conferences
+        </h3>
+        <p className="text-xl text-gray-600">
+          Get our latest conference schedules, updates, and insights straight to your inbox.
+        </p>
+      </div>
+      <div className="flex flex-col  space-y-2 max-w-[600px]">
+        <p className="text-xl text-custom-blue font-semibold">Work Email</p>
+        <div className="flex items-center space-x-4 mb-2">
+          <input 
+            type="email" 
+            placeholder="Enter your work email" 
+            className="p-2 border border-gray-300 rounded-md flex-1"
+          />
+          <button className="bg-custom-blue text-white px-4 py-2 rounded-md">
+            Continue
+          </button>
+        </div>
+        <p className="text-sm text-gray-600 max-w-full">
+          By clicking the "Subscribe" button, you are agreeing to the Gartner Terms of Use and Privacy Policy.
+        </p>
+      </div>
+    </div>
+    <div className="space-y-6 px-64 py-24 ">
+      <h2 className="text-5xl font-semibold text-gray-800 mb-4">Find your conference</h2>
+      <button className="bg-custom-blue text-white px-6 py-2 rounded-md hover:bg-blue-700" 
+        onClick={handleAddConferenceClick}>
+          Add Conference
+      </button>
+      <ul className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+        {conferences.map((conference) => (
+          <li 
+            key={conference.id} 
+            className="bg-white rounded-lg shadow-md overflow-hidden min-h-[185px] min-w-[280px] flex flex-col justify-between hover:bg-gray-200"
+
           >
-            <div>
-              <h3 className="text-2xl mb-2 text-custom-blue font-semibold">
-                {conference.title}
-              </h3>
-              <p className="text-lg text-gray-600">{formatDate(conference.date)}</p>
-            </div>
-            <div className="flex items-center mt-auto justify-between">
+            <Link 
+              to={`/conferences/${conference.id}`} 
+              className="block p-4 flex flex-col justify-between h-full"
+            >
+              <div>
+                <h3 className="text-2xl mb-2 text-custom-blue font-semibold">
+                  {conference.title}
+                </h3>
+                <p className="text-lg text-gray-600">{formatDate(conference.date)}</p>
+              </div>
+              <div className="flex items-center mt-auto justify-between">
                 <p className="text-lg text-gray-600">{conference.location}</p>
                 <svg width="14" height="14" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" clip-rule="evenodd" d="M1.80338 0.0627441L15.8034 0.0627441V3.06274H15.8033L15.8033 14.0628H12.8033L12.8033 5.25521L2.12132 15.9372L0 13.8159L10.7532 3.06274L1.80338 3.06274L1.80338 0.0627441Z" fill="#0000D3"></path>
                 </svg>
               </div>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </div>
-  <ConferenceModal 
-        isOpen={isModalOpen} 
-        onClose={handleCloseModal} 
-        onSave={handleSaveConference} 
-      />
-    </>
-
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <ConferenceModal 
+      isOpen={isModalOpen} 
+      onClose={handleCloseModal} 
+      onSave={handleSaveConference} 
+    />
+  </>
   );
 };
 
